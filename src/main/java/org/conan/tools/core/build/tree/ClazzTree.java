6e84b9e351f468d4eb99160d05b9e8288b277cf0
Conan Zhang <bsspirit@163.com>
2011-10-10 14:35:00 +0800

    tree

---
+++ src/main/java/org/conan/tools/core/build/tree/ClazzTree.java
@@ -0,0 +1,149 @@
+package org.conan.tools.core.build.tree;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.conan.tools.core.model.PackagePO;
+import org.conan.tools.util.match.StringMatch;
+
+/**
+ * 
+ * @author Conan
+ */
+public class ClazzTree extends PackageTree {
+
+    protected String model;
+
+    public ClazzTree(String model, PackagePO po) {
+        super(po);
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getDAOFile() {
+        return getDAOFolder() + File.separator + getDAOClazz();
+    }
+
+    @Deprecated
+    public String getIbatisFile() {
+        return getIbatisFolder() + File.separator + getIbatisClazz();
+    }
+
+    @Deprecated
+    public String getIbatisTestFile() {
+        return getIbatisTestFolder() + File.separator + getIbatisTestClazz();
+    }
+
+    public String getIbatisSQLFile() {
+        return getDAOFolder() + File.separator + getIbatisSQLClazz();
+    }
+
+    public String getServiceFile() {
+        return getServiceFolder() + File.separator + getServiceClazz();
+    }
+
+    public String getServiceImplFile() {
+        return getServiceImplFolder() + File.separator + getServiceImplClazz();
+    }
+
+    public String getModelFile() {
+        return getModelFolder() + File.separator + getModelClazz();
+    }
+
+    public String getModelModuleFile() {
+        return getModelFolder() + File.separator + getModelModuleClazz();
+    }
+
+    public String getWebFormFile() {
+        return getFormWebFolder() + File.separator + getWebFormClazz();
+    }
+
+    public String getDAOPackageClazz() {
+        return getDAOPackage() + PackageTree.POINT + getModel() + DAO;
+    }
+
+    public String getServicePackageClazz() {
+        return getServicePackage() + PackageTree.POINT + getModel() + SERVICE;
+    }
+
+    @Deprecated
+    public String getIbatisPackageClazz() {
+        return getIbatisPackage() + PackageTree.POINT + getModel() + DAO_IBATIS;
+    }
+
+    public String getModelPackageClazz() {
+        return getModelPackage() + PackageTree.POINT + getModel() + DTO;
+    }
+
+    public String getDAOClazz() {
+        return getModel() + DAO + JAVA_FILE;
+    }
+
+    @Deprecated
+    public String getIbatisClazz() {
+        return getModel() + DAO_IBATIS + JAVA_FILE;
+    }
+
+    @Deprecated
+    public String getIbatisTestClazz() {
+        return getModel() + DAO_IBATIS_TEST + JAVA_FILE;
+    }
+
+    public String getServiceClazz() {
+        return getModel() + SERVICE + JAVA_FILE;
+    }
+
+    public String getServiceImplClazz() {
+        return getModel() + SERVICE_IMPL + JAVA_FILE;
+    }
+
+    public String getWebFormClazz() {
+        return getModel() + FORM + JAVA_FILE;
+    }
+
+    public String getIbatisSQLClazz() {
+        return getModelLowercase() + XML_FILE;
+    }
+
+    public String getModelClazz() {
+        return getModel() + DTO + JAVA_FILE;
+    }
+
+    public String getModelModuleClazz() {
+        return StringMatch.first2Uppercase(model) + MODULE_DTO + JAVA_FILE;
+    }
+
+    public String getModelLowercase() {
+        return StringMatch.first2Lowercase(model);
+    }
+
+    public List<String> getTreeFile() {
+        List<String> tree = new ArrayList<String>();
+        tree.add(getDAOFile());
+        tree.add(getIbatisSQLFile());
+        tree.add(getModelFile());
+        tree.add(getWebFormFile());
+        tree.add(getServiceFile());
+        tree.add(getServiceImplFile());
+        // tree.add(getIbatisFile());
+        // tree.add(getIbatisTestFile());
+        return tree;
+    }
+
+    public String printTreeClazz() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getDAOClazz() + "\n");
+        sb.append(getIbatisSQLClazz() + "\n");
+        sb.append(getModelClazz() + "\n");
+        sb.append(getServiceClazz() + "\n");
+        sb.append(getServiceImplClazz() + "\n");
+        sb.append(getWebFormClazz() + "\n");
+        // sb.append(getIbatisClazz() + "\n");
+        // sb.append(getIbatisTestClazz() + "\n");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/org/conan/tools/core/build/tree/ResourceTree.java
@@ -0,0 +1,68 @@
+package org.conan.tools.core.build.tree;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.conan.tools.core.model.FilePO;
+
+public class ResourceTree extends FolderTree {
+
+    public ResourceTree(FilePO po) {
+        super(po);
+    }
+
+    // file name ///////////////////////////////////////////////////
+    public String getSQLCreateName() {
+        return "create" + SQL_FILE;
+    }
+
+    public String getSQLDropName() {
+        return "drop" + SQL_FILE;
+    }
+
+    public String getMybatisName() {
+        return "mybatis" + XML_FILE;
+    }
+
+    public String getSpringDAOName() {
+        return "spring-dao" + XML_FILE;
+    }
+
+    public String getSpringServiceName() {
+        return "spring-service" + XML_FILE;
+    }
+
+    // file folder /////////////////////////////////////////////////
+    public String getSQLCreateFile() {
+        return getSQLFolder() + File.separator + getSQLCreateName();
+    }
+
+    public String getSQLDropFile() {
+        return getSQLFolder() + File.separator + getSQLDropName();
+    }
+
+    public String getMybatisFile() {
+        return getResourceFolder() + File.separator + getMybatisName();
+    }
+
+    public String getSpringDAOFile() {
+        return getResourceFolder() + File.separator + getSpringDAOName();
+    }
+
+    public String getSpringServiceFile() {
+        return getResourceFolder() + File.separator + getSpringServiceName();
+    }
+
+    // file tree ///////////////////////////////////////////////////////////////
+    public List<String> getTreeFile() {
+        List<String> tree = new ArrayList<String>();
+        tree.add(getSQLCreateFile());
+        tree.add(getSQLDropFile());
+        tree.add(getMybatisFile());
+        tree.add(getSpringDAOFile());
+        tree.add(getSpringServiceFile());
+        return tree;
+    }
+
+}
