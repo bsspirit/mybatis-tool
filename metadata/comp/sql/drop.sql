4fcde2e043d9b2dffd042b41260a478b09277c91
bsspirit <bsspirit@163.com>
2013-04-24 22:56:40 +0800

    ape-mining

--- metadata/comp/sql/drop.sql
+++
@@ -1,9 +0,0 @@
-#This is competition DROP SQL.
-#@author Conan Zhang
-#@date 2012-07-27
-
-use competition;
-
-DROP TABLE IF EXISTS t_quiz;
-DROP TABLE IF EXISTS t_quiz_data;
-DROP TABLE IF EXISTS t_quiz_submit;
